f45294a854ab50c495a3efb7b41b099c2b441b45
avaneehirapara <avnihirapara0706@gmail.com>
2022-07-18 11:38:35 +0530

    r

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import Home from './app/container/Home'
 import City from './app/container/City'
+import Country from './app/container/Country'
 
 export default function App() {
   return (
     <View>
-    <Home />
-    <City />
+      <Country />
     </View>
   )
 }
---
+++ app/container/Country.js
@@ -0,0 +1,21 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import City from './City';
+
+export default function Country() {
+    const [countryName, setCountryName] = useState('India');
+
+    const cityChange = () => {
+        setCountryName('US');
+    }
+
+    return (
+        <View>
+            <Text>Country: {countryName}</Text>
+            <TouchableOpacity onPress={cityChange}>
+                <Text>Change</Text>
+            </TouchableOpacity>
+            <City country_name={countryName} />
+        </View>
+    )
+}
